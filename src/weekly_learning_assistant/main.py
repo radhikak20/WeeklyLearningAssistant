9faeffc7074f361a784e402266b7d41311f614bb
radhikaK2018 <radhika.a.kamat@gmail.com>
2026-04-16 13:08:15 -0500

    Add files via upload

---
+++ src/weekly_learning_assistant/main.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import sys
+from weekly_learning_assistant.crew import WeeklyLearningAssistantCrew
+
+# This main file is intended to be a way for your to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'learning_topic': 'sample_value',
+        'skill_level': 'sample_value',
+        'available_hours': 'sample_value',
+        'learning_goals': 'sample_value',
+        'previous_week_progress': 'sample_value'
+    }
+    WeeklyLearningAssistantCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'learning_topic': 'sample_value',
+        'skill_level': 'sample_value',
+        'available_hours': 'sample_value',
+        'learning_goals': 'sample_value',
+        'previous_week_progress': 'sample_value'
+    }
+    try:
+        WeeklyLearningAssistantCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        WeeklyLearningAssistantCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        'learning_topic': 'sample_value',
+        'skill_level': 'sample_value',
+        'available_hours': 'sample_value',
+        'learning_goals': 'sample_value',
+        'previous_week_progress': 'sample_value'
+    }
+    try:
+        WeeklyLearningAssistantCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: main.py <command> [<args>]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command == "run":
+        run()
+    elif command == "train":
+        train()
+    elif command == "replay":
+        replay()
+    elif command == "test":
+        test()
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
